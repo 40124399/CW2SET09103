99a834d933e1a7f90dfd9bc1119cda2e0a406243
40124399 <hoijf666@gmail.com>
2015-11-26 11:21:06 +0000

    upload still not working..

--- schema.sql
+++ schema.sql
@@ -1,7 +1,19 @@
 DROP TABLE if EXISTS user;
 
 CREATE TABLE user (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY,
     email text NOT NULL,
     username text NOT NULL,
     password text NOT NULL);
+
+DROP TABLE if EXISTS songs;
+
+CREATE TABLE songs (
+    songID INTEGER NOT NULL PRIMARY KEY,
+    id INTEGER NOT NULL,
+    path text NOT NULL,
+    title text NOT NULL,
+    artist text,
+    album text,
+    genre text);
+
